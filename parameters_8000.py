e552c05b8f1f3fdfb0ae00fd4d9f4f3a91c18c44
Cameron Mott <cmott@uptane.org>
2017-01-12 17:11:07 -0500

    merge

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,13 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$af4d6bff8eea33e0$b6790d73c40ffabb230241f3a83d475f0ac87e04"
-=======
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$95551ffd3ac2c734$e30f97811763a36dbabed2263cf911292c00ee40"
-=======
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$adbe1199ae12a0bb$f7d99897412c86f8fe6e4eddc825b0e081d6d679"
-=======
-password="pbkdf2(1000,20,sha512)$861b1876f5b78358$c1532a687ff2a4b2febd20f78529bc8e73753235"
->>>>>>> 7b4701b1f50c50506af18f5197b00fa98ac9d81c
->>>>>>> 5f3ef725cc92e270880258740a462cf61f8c066f
->>>>>>> de288497278751e21fe0873003944b7b30b86bf1
+password="pbkdf2(1000,20,sha512)$b953adb96f4d751e$0c9005c9f6504d59b527e9c775be132bf45f9f6a"
