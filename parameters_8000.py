fc8e42df7b68a84d64cb577ab604602a040e9570
Allen Cain <edward.cain@swri.org>
2017-01-17 09:02:59 -0600

    Fix issues commit?

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,9 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$92427c5ba92f1cab$2676e689db019e03fea6f7c8b3a5c1d9e0c640f2"
-=======
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$b953adb96f4d751e$0c9005c9f6504d59b527e9c775be132bf45f9f6a"
-=======
-password="pbkdf2(1000,20,sha512)$88b2402dcb24d413$5e223d61c501affda174ce52dcb87e10f5c5c6db"
->>>>>>> c30ce935265801d669ea4774a66c797e7766e500
->>>>>>> b24741dc7712bdca9e8809b5bf2887f8e88542c5
+password="pbkdf2(1000,20,sha512)$a9d7e54ec03af1c5$8a23457cac79eeb816247bbc8a7c9dcd4e1d8b76"
