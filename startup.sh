35fd9b663ee0c0a4540c5fdb1e0f5846f84af111
Cameron Mott <cmott@uptane.org>
2017-02-16 17:19:30 -0500

    merge from repo side

--- startup.sh
+++ startup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-nohup python3 web2py.py -a 'uptane' -c server.crt -k server.key -i 127.0.0.1 -p 8000 &
+nohup python3 web2py.py -a 'SO14uptane' -c server.crt -k server.key -i 127.0.0.1 -p 8000 &
