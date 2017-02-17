60ab02a4641a4528783595b126fd93b0b6603a15
Allen Cain <edward.cain@swri.org>
2017-02-17 09:00:12 -0600

    Cleaned up more code.  Added more comments.  Removed 'Edit' issue on hacked screens showing the ECU ids and not their type:version, Added functionality that when a vehicle sends a vehicle manifest, the checkin date for the vehicle updates and is reflected in the 'Time Elapsed' column on the OEM screen.  Next is to implement error checking when the OEM user tries to view the vehicle data (via button click) without first selecting a vehicle.  Afterwards will need to implemenet Reset Everything functionality.

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$9283838ab206fc8e$33df0eae08407b80b6bed7caf18a9ea0ad6b2310"
-=======
-password="pbkdf2(1000,20,sha512)$8c02f589b967cd32$2901300c06649a450fe5063ff0660fdde6839fa8"
->>>>>>> 8ded29ea30e40774609612da66fa15fa2e4518bf
+password="pbkdf2(1000,20,sha512)$8b85435d052cae47$ab2ff001d32439d5017e5fcd4ea9fa0a9098ca90"
